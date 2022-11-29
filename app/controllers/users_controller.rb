279f8c663e3407958cb5991afe50ed556dd69bcc
Rhoda Gachimu <gachimurhoda@gmail.com>
2022-11-29 10:15:29 +0300

    done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+    def show
+        user = User.find(params[:id])
+        render json: user, include: :items
+      rescue ActiveRecord::RecordNotFound
+        render json: { error: "Record Not Found" }, status: :not_found
+      end
 end
